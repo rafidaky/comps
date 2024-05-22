ae5a0d2c59ebfd5e042ba59bf29a9e185c0da811
rafidaky <rafidakn@gmail.com>
2024-05-22 22:02:41 +0200

    custom button comp final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 import Button from "./Button";
 
-
 function App() {
+  const handleClick = () => {
+    console.log("Click!!");
+  };
+
   return (
     <div>
       <div>
-        <Button success outline rounded>
+        <Button className="mb-5" success outline rounded onClick={handleClick}>
+          <GoBell />
           Click me!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
