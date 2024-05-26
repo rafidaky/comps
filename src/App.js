809419983afc8730a6a77cfccb48e1a7f32ee504
rafidaky <rafidakn@gmail.com>
2024-05-26 19:52:41 +0200

    dropdown final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import Accordion from "./components/Accordion";
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "hg34h3",
-      label: "Can I use React on a project?",
-      content: "You can use React on any project you want.",
-    },
-    {
-      id: "jh232l",
-      label: "Can I use Javascript on a project?",
-      content: "You can use Javascript on any project you want.",
-    },
-    {
-      id: "kujy23s1",
-      label: "Can I use CSS on a project?",
-      content: "You can use CSS on any project you want.",
-    },
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
-  return <Accordion items={items} />;
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,24 @@
+import Accordion from "./components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "hg34h3",
+      label: "Can I use React on a project?",
+      content: "You can use React on any project you want.",
+    },
+    {
+      id: "jh232l",
+      label: "Can I use Javascript on a project?",
+      content: "You can use Javascript on any project you want.",
+    },
+    {
+      id: "kujy23s1",
+      label: "Can I use CSS on a project?",
+      content: "You can use CSS on any project you want.",
+    },
+  ];
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
