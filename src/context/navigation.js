e609293fd561ec65ec9bc1e4362db5799e05eceb
rafidaky <rafidakn@gmail.com>
2024-05-30 15:59:49 +0200

    manual navigation impl start

---
+++ src/context/navigation.js
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+
+const NavigationContext = createContext();
+
+function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handler);
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
+  return (
+    <NavigationContext.Provider value={{}}>
+      {children}
+    </NavigationContext.Provider>
+  );
+}
+
+export { NavigationProvider };
+export default NavigationContext;
